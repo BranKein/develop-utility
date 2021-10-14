5562a82d4d3597de66a0cfdbf1bc8dd6e2667957
YeonhyukKim <brankein13@gm.gist.ac.kr>
2021-10-14 14:30:03 +0900

    Initial commit

---
+++ src/main/java/com/yhkim/developutility/YhkimDevelopUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.yhkim.developutility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhkimDevelopUtilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YhkimDevelopUtilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhkim/developutility/YhkimDevelopUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhkim.developutility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhkimDevelopUtilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
